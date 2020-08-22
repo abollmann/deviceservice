6a46d7bcc0d596802745ffbf6c32fc2cda24ec23
Aron <aron.bollmann@web.de>
2020-08-22 13:46:39 +0200

    test wrong host

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from deviceservice import app
 from deviceservice.consumer import ApartmentCommandConsumer
 
 from config import APP_HOST
+from deviceservice.fraunhofer_api_listener import update_devices
 
 if __name__ == '__main__':
     ApartmentCommandConsumer().start()
+    update_devices()
     app.run(host=APP_HOST, port=5002)
